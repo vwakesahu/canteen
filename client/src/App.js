96a241cc3315516e4b39d28ae88e02f0e5bcdeac
vwakesahu <viveksahu1762@gmail.com>
2023-12-28 20:50:12 +0530

    Removed Router from App.js and moved it to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,13 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { LoginContainer, MainContainer } from "./components";
 const App = () => {
   return (
     <div>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginContainer />} />
-          <Route path="/" element={<MainContainer />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/login" element={<LoginContainer />} />
+        <Route path="/" element={<MainContainer />} />
+      </Routes>
     </div>
   );
 };
