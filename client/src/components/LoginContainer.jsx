a1793a7a288f17b7e06f2a508cfd0b903b50c875
vwakesahu <viveksahu1762@gmail.com>
2024-01-11 22:12:33 +0530

    UI of login container

--- client/src/components/LoginContainer.jsx
+++ client/src/components/LoginContainer.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
+import { BgLogin } from "../assets";
+import LogoContainer from "./LogoContainer";
+import InputContainer from "./InputContainer";
 
 const LoginContainer = () => {
-  return (
-    <div>LoginContainer</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-export default LoginContainer
+  return (
+    <div className="w-screen h-screen relative overflow-hidden flex">
+      <img
+        src={BgLogin}
+        alt="login bg image"
+        className="w-full h-full object-cover"
+      />
+      <div className="absolute w-[80%] h-full md:w-508  bg-opacity-60 backdrop-filter backdrop-blur-md bg-white px-12 flex flex-col items-center gap-2">
+        <LogoContainer />
+
+        <div className="mt-16">
+          <p className="text-[1.5rem] text-center mb-10">
+            Savor the Flavor: Log in for a Taste Adventure! 🌮🔑
+          </p>
+          <form action="" className="flex flex-col gap-2">
+            <InputContainer
+              type="email"
+              placeholder="Enter Email"
+              onChange={(e) => setEmail(e.value)}
+              value={email}
+            />
+            <InputContainer
+              type="text"
+              placeholder="Enter Password"
+              onChange={(e) => setPassword(e.value)}
+              value={password}
+            />
+            <button
+              onClick={(e) => {
+                e.preventDefault();
+                console.log("Email: ", email, "Password: ", password);
+              }}
+              className="bg-red-700 text-white py-2 rounded-lg mt-4"
+            >
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginContainer;
