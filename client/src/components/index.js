11d9e31052803c84b02e6901b0590817f301c1a4
vwakesahu <viveksahu1762@gmail.com>
2024-01-11 22:11:22 +0530

    Optimizing imports

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as LoginContainer } from "./LoginContainer";
 export { default as MainContainer } from "./MainContainer";
 export { default as LogoContainer } from "./LogoContainer";
+export { default as InputContainer } from "./InputContainer";
